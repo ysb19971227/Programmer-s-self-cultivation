6fc88cf248ff0e245045d1ccf844863beb60cf6d
ysb19971227 <69384148+ysb19971227@users.noreply.github.com>
2021-03-07 11:34:10 +0800

    Add files via upload

---
+++ test_03-06/src/HelloWorld.java
@@ -0,0 +1,18 @@
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//    }
+//}
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        byte a = 5;
+        short b = 10;
+        int c = 15;
+        long d = 20;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+    }
+}
